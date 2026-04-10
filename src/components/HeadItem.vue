36975d6d11739c4c33907537f399ba68bd54499c
jeefyang <jeefyang@sina.com>
2026-04-10 10:24:34 +0800

    增加强制刷新功能

--- src/components/HeadItem.vue
+++ src/components/HeadItem.vue
@@ -6,7 +6,20 @@
     </n-flex>
 
     <div style="flex: 1; text-align: center">{{ dataStore?.pageData?.config?.title }}</div>
-    <n-flex style="flex: 1" justify="end">
+    <n-flex style="flex: 1; flex-wrap: nowrap" justify="end">
+      <!-- 强制刷新按钮 -->
+      <n-icon class="mr-2" size="20" @click="toForceUpdate">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M17.65 6.35A7.958 7.958 0 0 0 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            fill="currentColor"
+          ></path>
+        </svg>
+      </n-icon>
       <!-- 添加按钮 -->
       <n-icon class="mr-2" size="20" @click="toActiveAddPage">
         <svg
@@ -362,6 +375,15 @@ const toDelPage = async () => {
   msg.success(res1.msg)
   active_config.value = false
   loading.value = false
+}
+
+const toForceUpdate = async () => {
+  const res = await dataStore.updatePageData()
+  if (res.code == 200) {
+    res.msg && msg.success(res.msg)
+    return
+  }
+  res.msg && msg.error(res.msg)
 }
 
 const unSelectTypes = () => {
